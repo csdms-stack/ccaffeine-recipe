24029222eb003dbb0e79fd2759370e865a7c5f2e
mcflugen <mcflugen@gmail.com>
2017-12-11 21:53:25 -0700

    Use --std=c++11 option for g++.

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ if [ ! -d "$PREFIX/lib64" ]; then
   ln -s "$PREFIX/lib" "$PREFIX/lib64"
 fi
 
-export CXXFLAGS="--std=c++14"
+export CXXFLAGS="--std=c++11"
 
 ./configure --prefix=$PREFIX --without-mpi --with-boost=$PREFIX/include
 make
